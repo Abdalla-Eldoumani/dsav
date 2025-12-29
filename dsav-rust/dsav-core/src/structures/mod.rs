ae622a35c407a8fcbcffae6d7b8bbcb8c1f633d9
Abdalla Eldoumani <aamsdoumani@gmail.com>
2025-12-28 20:48:01 -0700

    Export Red-Black Tree module

--- dsav-rust/dsav-core/src/structures/mod.rs
+++ dsav-rust/dsav-core/src/structures/mod.rs
@@ -5,9 +5,11 @@ pub mod stack;
 pub mod queue;
 pub mod linked_list;
 pub mod bst;
+pub mod rb_tree;
 
 pub use array::VisualizableArray;
 pub use stack::VisualizableStack;
 pub use queue::VisualizableQueue;
 pub use linked_list::VisualizableLinkedList;
 pub use bst::VisualizableBST;
+pub use rb_tree::VisualizableRBTree;
